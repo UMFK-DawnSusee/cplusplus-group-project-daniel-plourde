7c6554258b7729335dd5616ccd54f13869970059
Dawn Susee <dawnsusee@gmail.com>
2017-09-01 07:59:34 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Dawn
+ *
+ * Created on September 1, 2017, 7:54 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Let's play Tic Tac Toe!";
+    return 0;
+}
+
